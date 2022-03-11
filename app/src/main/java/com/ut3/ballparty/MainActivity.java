170269a82e38801e2e986120d8a622a17df8f57a
LoreaRedondo <lorearedondouniv@gmail.com>
2022-03-11 09:11:01 +0100

    Principal screen

--- app/src/main/java/com/ut3/ballparty/MainActivity.java
+++ app/src/main/java/com/ut3/ballparty/MainActivity.java
@@ -1,17 +1,37 @@
 package com.ut3.ballparty;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ut3.ballparty.game.GameActivity;
 
 public class MainActivity extends Activity {
+
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.img);
+
+        TextView scoreValue = findViewById(R.id.scoreValue);
+        SharedPreferences sharedScore = this.getPreferences(Context.MODE_PRIVATE);
+        int score = sharedScore.getInt("score",0);
+        scoreValue.setText(String.valueOf(score));
+
     }
 
-    public void startGame(){
+    public void startGame(View view) {
         //ICI CHANGER ACTIVITE
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/ut3/ballparty/game/GameActivity.java
+++ app/src/main/java/com/ut3/ballparty/game/GameActivity.java
@@ -9,7 +9,7 @@ public class GameActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(R.id.button);
+        findViewById(R.id.jouer);
         setContentView(new GameView(this));
     }
 }
