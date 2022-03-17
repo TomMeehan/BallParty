1fa2363342079d1435db688c2eac2d72097427fd
Maxime Colin <maximecolin47@protonmail.com>
2022-03-17 17:58:37 +0100

    Fix fullscreen

--- app/src/main/java/com/ut3/ballparty/game/RulesActivity.java
+++ app/src/main/java/com/ut3/ballparty/game/RulesActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.ut3.ballparty.MainActivity;
 import com.ut3.ballparty.R;
@@ -13,6 +15,11 @@ public class RulesActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_rules);
     }
 
